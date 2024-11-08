c8be0f101be66f1dd74c5c721699961305698370
sheyla Daza <dazashe28@gmail.com>
2024-11-08 18:52:29 -0500

    estructura detalle-forms

--- my-app/app/detalle/page.jsx
+++ my-app/app/detalle/page.jsx
@@ -1,8 +1,9 @@
 "use client"
-import CardQuestionComponent from '../componentes/CardQuestionComponent';
+import CardQuestionComponent from '../components/CardQuestionComponent';
+import { SiGoogleforms } from "react-icons/si";
 const detallePage = () => {
     return(
-<div className="w-full h-full bg-gray-200 flex flex-col ">
+<div className="w-full h-full bg-gray-200 flex flex-col relative">
 <div className="w-full h-32 bg-white shadow-lg flex items-center justify-center mb-[40px]">
 <h1 className="text-4xl">Formulario generado ✨ </h1>
 </div>
@@ -11,10 +12,11 @@ const detallePage = () => {
 <CardQuestionComponent />
 <CardQuestionComponent />
 </div>
+<div className="sticky bottom-0 flex justify-center p-4">
+<button className='bg-violet-700 m-[20px] border rounded-full w-[200px] h-[70px] text-white flex justify-center flex-row content-center flex-wrap text-2xl gap-[10px]'> generar <SiGoogleforms /></button>
+</div>
 </div>
     )
 }
-
-
 
 export default  detallePage
