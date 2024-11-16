3f8017d68e5415e901aaf704fd91f61fc7401662
Camila Pua <camilapua7@gmail.com>
2024-11-16 11:25:54 -0500

    Update home page

--- src/app/homeLayout.jsx
+++ src/app/homeLayout.jsx
@@ -1,14 +1,14 @@
 "use client";
 
-// import PromptInputComponent from "../components/PromptInputComponent";
-// import PromptTitleComponent from "../components/PromptTitleComponent";
+import PromptInput from "@/components/PromptInput";
+import PromptTitle from "@/components/PromptTitle";
 import AddQuestion from "@/components/AddQuestion";
 
 const HomeLayout = () => {
   return (
     <div className="flex flex-col items-center gap-y-5 h-max content-center">
-      {/* <PromptTitleComponent />
-      <PromptInputComponent /> */}
+      <PromptTitle />
+      <PromptInput />
       <AddQuestion />
     </div>
   );
---
+++ src/components/PromptInput.jsx
@@ -0,0 +1,12 @@
+const PromptInput = () => {
+  return (
+    <div>
+      <input
+        placeholder="Ingresa tu prompt"
+        className="opacity-30 border-none rounded-md text-black p-4 w-96"
+      />
+    </div>
+  );
+};
+
+export default PromptInput;
