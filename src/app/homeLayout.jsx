643a39b58d73651e92ff818943dfe142c7085342
rick <rick0709xe@gmail.com>
2024-11-23 10:07:55 -0500

    se modifica el input para que ya guarde variables que se le ingrese

--- src/app/homeLayout.jsx
+++ src/app/homeLayout.jsx
@@ -4,6 +4,7 @@ import PromptInput from "@/components/PromptInput";
 import PromptTitle from "@/components/PromptTitle";
 import AddQuestion from "@/components/AddQuestion";
 
+
 const HomeLayout = () => {
   return (
     <div className="flex flex-col items-center gap-y-5 h-max content-center">
