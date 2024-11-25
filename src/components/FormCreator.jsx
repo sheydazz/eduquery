bced1a5251d84346d17580230ce077063dff6680
Camila Pua <camilapua7@gmail.com>
2024-11-24 19:43:26 -0500

    Add update a google form functionality

--- src/components/FormCreator.jsx
+++ src/components/FormCreator.jsx
@@ -1,9 +1,61 @@
 import React from "react";
 import { useState } from "react";
 import { SiGoogleforms } from "react-icons/si";
+import { formatformInfo } from "@/lib/format-items";
+import { formatQuestion } from "@/lib/format-items";
+
+// Harcode data for tests
+const dataFakeAI = [
+  {
+    formTitle: "Cuestionario de Ciencias",
+    formDescription: "Responde las siguientes preguntas para evaluar tus conocimientos científicos."
+  },
+  [
+    {
+      title: "¿Cuál es el símbolo químico del agua?",
+      type: "RADIO",
+      correctAnswer: "H2O",
+      options: [{'value': 'H2O'}, {'value': 'CO2'}, {'value': 'O2'}, {'value': 'H2'}]
+    },
+    {
+      title: "¿Quién formuló la teoría de la relatividad?",
+      type: "RADIO",
+      correctAnswer: "Albert Einstein",
+      options: [{'value': 'Isaac Newton'}, {'value': 'Albert Einstein'}, {'value': 'Galileo Galilei'}, {'value': 'Niels Bohr'}]
+    },
+    {
+      title: "¿Cuál es el planeta más cercano al Sol?",
+      type: "RADIO",
+      correctAnswer: "Mercurio",
+      options: [{'value': 'Venus'}, {'value': 'Tierra'}, {'value': 'Marte'}, {'value': 'Mercurio'}]
+    },
+    {
+      title: "¿Qué gas es el más abundante en la atmósfera terrestre?",
+      type: "RADIO",
+      correctAnswer: "Nitrógeno",
+      options: [{'value': 'Oxígeno'}, {'value': 'Dióxido de carbono'}, {'value': 'Nitrógeno'}, {'value': 'Argón'}]
+    },
+    {
+      title: "¿Cuál es la fórmula química del metano?",
+      type: "RADIO",
+      correctAnswer: "CH4",
+      options: [{'value': 'C2H6'}, {'value': 'CH4'}, {'value': 'CO'}, {'value': 'H2O'}]
+    }
+  ]
+]
+
+
+const formatedInfo = formatformInfo(dataFakeAI[0]);
+const formatedQuestions = [];
+
+dataFakeAI[1].forEach((question) => {
+  formatedQuestions.push(formatQuestion(question));
+});
+
 const FormCreator = ({ onFormCreated }) => {
   const [formResponse, setFormResponse] = useState(null);
 
+  // Create form
   const handleCreateForm = async () => {
     try {
       // Haciendo la solicitud POST a la API
@@ -12,8 +64,17 @@ const FormCreator = ({ onFormCreated }) => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ title: "dev test eduquery" }),
+        body: JSON.stringify({
+          info: formatedInfo,
+          settings: {
+            quizSettings: {
+              isQuiz: true,
+            },
+          },
+          items: formatedQuestions,
+        }),
       });
+      
 
       if (!response.ok) {
         throw new Error("Error al crear el formulario");
