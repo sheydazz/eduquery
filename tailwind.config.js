9e9e0b6a7954c23988153583cd4945b5465c6934
Ricardo <rick0709xe@gmail.com>
2024-12-13 12:17:38 -0500

    se mejora el titulo de la pagina para que este animado y con colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,16 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      keyframes: {
+        gradient: {
+          "0%": { backgroundPosition: "0% 50%" },
+          "50%": { backgroundPosition: "100% 50%" },
+          "100%": { backgroundPosition: "0% 50%" },
+        },
+      },
+      animation: {
+        gradient: "gradient 3s ease infinite",
+      },
     },
   },
   plugins: [],
