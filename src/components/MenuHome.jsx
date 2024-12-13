01ba8488fd4accff3fd02dc30a9bc3189052dd8b
Ricardo <rick0709xe@gmail.com>
2024-12-13 15:45:30 -0500

    cambios en el boton de generar (solo estilos)

--- src/components/MenuHome.jsx
+++ src/components/MenuHome.jsx
@@ -7,10 +7,12 @@ const MenuHome = () => {
     const router = useRouter();
 
     return (
-        <div className="w-80 h-screen bg-gray-900 flex flex-col items-center justify-center">
-            <div className="flex justify-center mb-8">
-                <span>Logo</span>
-            </div>
+        <div className="w-80 h-screen bg-gray-800 flex flex-col items-center justify-center">
+           <div className="flex justify-center mb-8">
+                <img 
+                    className="w-full h-full object-contain max-w-[100px] max-h-[100px]" 
+                    src="https://files.oaiusercontent.com/file-GQJiRTphhvzgPc6D5QgdTy?se=2024-12-13T20%3A39%3A01Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3D2b5ffc01-8348-4e25-9c91-093f70550502.webp&sig=ZPm5UXHA1FXHdInqmQjj0JOVArY6qgGfLKVnd/0UUIM%3D"/>
+                </div>
             <div className="w-1/2 border-b border-gray-600 mb-12"></div>
             <div className="flex flex-col items-center space-y-6">
                 <ButtonLogin
--- src/components/PromptTitle.jsx
+++ src/components/PromptTitle.jsx
@@ -1,10 +1,10 @@
 const promptTitle = () => {
   return (
     <div className="flex flex-col items-center">
-      <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-500 animate-gradient bg-[length:200%_200%] mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+      <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-500 animate-gradient bg-[length:200%_200%] mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold ">
         Eduquery
       </h1>
-      <h6>Genera automáticamente tus formularios con AI ✨</h6>
+      <h6 className="font-monaco">Genera automáticamente tus formularios con AI ✨</h6>
     </div>
   );
 };
